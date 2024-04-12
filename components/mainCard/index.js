11b855f25d327532ef09ae6d98c10c14bc22a803
Palmieri Federico <palmierifederico3@gmail.com>
2024-04-12 18:57:25 +0200

    feat: aggiunto javascript alla mainCard

---
+++ components/mainCard/index.js
@@ -0,0 +1,52 @@
+const mainMenu = document.getElementById("mainMenu");
+const floorsCard = document.getElementById("floorsCard");
+const patiosCard = document.getElementById("patiosCard");
+const drivewaysCard = document.getElementById("drivewaysCard");
+const wallsCard = document.getElementById("wallsCard");
+const poolCard = document.getElementById("poolCard");
+const commercialCard = document.getElementById("commercialCard");
+const publicCard = document.getElementById("publicCard");
+
+floorsCard.addEventListener("mouseover", () => {
+  mainMenu.classList.toggle("imgFloorsCard");
+});
+floorsCard.addEventListener("mouseleave", () => {
+  mainMenu.classList.remove("imgFloorsCard");
+});
+
+patiosCard.addEventListener("mouseover", () => {
+  mainMenu.classList.toggle("imgPatiosCard");
+});
+patiosCard.addEventListener("mouseleave", () => {
+  mainMenu.classList.remove("imgPatiosCard");
+});
+drivewaysCard.addEventListener("mouseover", () => {
+  mainMenu.classList.toggle("imgDrivewaysCard");
+});
+drivewaysCard.addEventListener("mouseleave", () => {
+  mainMenu.classList.remove("imgDrivewaysCard");
+});
+wallsCard.addEventListener("mouseover", () => {
+  mainMenu.classList.toggle("imgWallsCard");
+});
+wallsCard.addEventListener("mouseleave", () => {
+  mainMenu.classList.remove("imgWallsCard");
+});
+poolCard.addEventListener("mouseover", () => {
+  mainMenu.classList.toggle("imgPoolCard");
+});
+poolCard.addEventListener("mouseleave", () => {
+  mainMenu.classList.remove("imgPoolCard");
+});
+commercialCard.addEventListener("mouseover", () => {
+  mainMenu.classList.toggle("imgCommercialCard");
+});
+commercialCard.addEventListener("mouseleave", () => {
+  mainMenu.classList.remove("imgCommercialCard");
+});
+publicCard.addEventListener("mouseover", () => {
+  mainMenu.classList.toggle("imgPublicCard");
+});
+publicCard.addEventListener("mouseleave", () => {
+  mainMenu.classList.remove("imgPublicCard");
+});
